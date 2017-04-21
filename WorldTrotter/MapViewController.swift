bcab5a2b27237e1bc86544e7775537fe302f03d3
Pavel_Bobkov <microsmaster@rambler.ru>
2017-04-22 01:27:42 +0300

    chapter 6 - programmatic views

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,11 +7,53 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+    }
+    
     override func viewDidLoad() {
         print("MapViewController loaded its view")
+    }
+    
+    func mapTypeChanged(_ segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = .standard
+            
+        case 1:
+            mapView.mapType = .hybrid
+            
+        case 2:
+            mapView.mapType = .satellite
+            
+        default:
+            break
+        }
     }
     
     
