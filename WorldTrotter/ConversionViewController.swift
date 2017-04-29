7568e232345e9ca19e2bc9374383c508491afd11
Pavel_Bobkov <microsmaster@rambler.ru>
2017-04-30 01:35:21 +0300

    chapter 7 - localization

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
     let numberFormatter: NumberFormatter = {
         let nf = NumberFormatter()
@@ -35,9 +36,13 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
+        textField.resignFirstResponder()
+    }
+    
     @IBAction func fahrenheitFieldEditingChaged(_ textField: UITextField){
-        if let text = textField.text, let value = Double(text) {
-            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        if let text = textField.text, let number = numberFormatter.number(from: text){
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
         }
         else{
             fahrenheitValue = nil
@@ -62,8 +67,11 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool{
         
-        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
-        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+        
+        let existingTextHasDecimalSeparator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
         
         if existingTextHasDecimalSeparator != nil,
             replacementTextHasDecimalSeparator != nil{
--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -17,7 +17,10 @@ class MapViewController: UIViewController {
         mapView = MKMapView()
         view = mapView
         
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        let standardString = NSLocalizedString("Standart", comment: "Standard map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let satelliteString = NSLocalizedString("Satellite", comment: "Satellite map view")
+        let segmentedControl = UISegmentedControl(items: [standardString, hybridString, satelliteString])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
